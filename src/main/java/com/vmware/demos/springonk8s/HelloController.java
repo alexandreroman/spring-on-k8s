3ce663db2de85bdb5486978d24e0474e76253547
Alexandre Roman <aroman@vmware.com>
2020-05-27 18:10:46 +0200

    Move HelloController class into its own file

--- src/main/java/com/vmware/demos/springonk8s/HelloController.java
+++ src/main/java/com/vmware/demos/springonk8s/HelloController.java
@@ -16,15 +16,15 @@
 
 package com.vmware.demos.springonk8s;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
-public class Application {
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+@RestController
+class HelloController {
+    @GetMapping(value = "/", produces = MediaType.TEXT_PLAIN_VALUE)
+    String greeting() {
+        // Just return a simple String.
+        return "Hello world!";
     }
 }
