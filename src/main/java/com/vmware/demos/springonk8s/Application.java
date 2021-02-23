989ec798b9e22a6f9eab77518924f5b346401a48
Alexandre Roman <aroman@vmware.com>
2021-02-23 12:38:34 +0100

    Clean up

--- src/main/java/com/vmware/demos/springonk8s/Application.java
+++ src/main/java/com/vmware/demos/springonk8s/Application.java
@@ -18,9 +18,6 @@ package com.vmware.demos.springonk8s;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class Application {
